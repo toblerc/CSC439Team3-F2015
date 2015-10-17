0c39eafcc6e530525a2c4e752cbec1b49a44d3c7
davisb10 <davisb10@mymail.nku.edu>
2015-10-17 08:06:45 -0400

    JUnit Testing Round 2

--- src/test/TestCacheList.java
+++ src/test/TestCacheList.java
@@ -1,20 +1,66 @@
 package test;
 import static org.junit.Assert.*;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+
 import main.CacheList;
 
 import org.junit.Test;
 
 //import org.junit.After;
-//import org.junit.AfterClass;
+import org.junit.AfterClass;
 import org.junit.Before;
-//import org.junit.BeforeClass;
-
+import org.junit.BeforeClass;
 
 public class TestCacheList {
 
 	String directory;
 	int maxSize;		
 	CacheList cltest;
+	
+	// Adds in the testDirectory and copies the input file
+	// into the directory for use later
+	@BeforeClass
+	public static void setUpOnce(){
+		String newDirectory = "C:\\testDirectory\\";
+		File file = new File(newDirectory);
+		if(!file.exists()){
+			file.mkdirs();
+			File source = new File("data\\input.txt");
+			File dest = new File(newDirectory + "input.txt");
+			try {
+				Files.copy(source.toPath(), dest.toPath());
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	//Delete testDirectory
+	@AfterClass
+	public static void tearDownOnce(){
+		String directory = "C:\\testDirectory\\";
+		System.out.println(deleteDirectory(new File(directory)));
+	}
+	
+	// Recursively delete the testDirectory and all files inside,
+	// thus leaving file system unchanged
+	private static boolean deleteDirectory(File path){
+		if(path.exists()){
+			File[] files = path.listFiles();
+			for (int i = 0; i < files.length; i++){
+				if(files[i].isDirectory()){
+					deleteDirectory(files[i]);
+				} else {
+					files[i].delete();
+				}
+			}
+		}
+		return (path.delete());
+	}
 	
 	@Before
 	public void setUp(){
